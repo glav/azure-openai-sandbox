d56db62325d98c86755c9c3ac85363d21892fa6e
Paul Glavich <glav@theglavs.com>
2023-04-20 08:46:57 +1000

    Added simple timings to see how long it took

--- OpenAIEngine.cs
+++ OpenAIEngine.cs
@@ -18,10 +18,12 @@ namespace azure_openai_sandbox
             var client = new OpenAIClient(new Uri(_config.Endpoint), new AzureKeyCredential(_config.ApiKey));
 
             string prompt = "Can you write a poem about unicorns and cats?";
-            Console.Write($"Input: {prompt}\n");
+            Console.WriteLine($"Input: {prompt}\n");
+            Console.WriteLine("[{0}].... calling open AI service.",DateTime.Now.ToString("hh:mm:ss.ffff"));
 
             Response<Completions> completionsResponse =
                 await client.GetCompletionsAsync(_config.ModelName, prompt);
+            Console.WriteLine("[{0}].... completed calling open AI service.\n",DateTime.Now.ToString("hh:mm:ss.ffff"));
             string completion = completionsResponse.Value.Choices[0].Text;
             Console.WriteLine($"Chatbot: {completion}");
         }
