fc951e56c1c562548ba6b80723c45295208da9ae
Paul Glavich <glav@theglavs.com>
2023-04-19 17:11:42 +1000

    Basic console project

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace azure_openai_sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Console with App Settings!");
+
+            var appConfig = LoadAppSettings();
+
+            if (appConfig == null)
+            {
+                Console.WriteLine("Missing or invalid appsettings.json...exiting");
+                return;
+            }
+
+            var configValue = appConfig["configValue"];
+			Console.WriteLine("Got config value: [{0}]",configValue);
+
+        }
+        static IConfigurationRoot LoadAppSettings()
+        {
+			var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            var appConfig = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{environmentName}.json", true, true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            return appConfig;
+        }
+    }
+
+}
